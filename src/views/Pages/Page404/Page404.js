e6513cfca79d45b331ad72b2959d2300b3204811
Jonathan Alexander <jalexander@thinksum.com>
2018-11-10 18:47:31 -0500

    Fix string on line 13

--- src/views/Pages/Page404/Page404.js
+++ src/views/Pages/Page404/Page404.js
@@ -10,7 +10,7 @@ class Page404 extends Component {
             <Col md="6">
               <div className="clearfix">
                 <h1 className="float-left display-3 mr-4">404</h1>
-                <h4 className="pt-3">Oops! You're lost.</h4>
+                <h4 className="pt-3">Oops! You are lost.</h4>
                 <p className="text-muted float-left">The page you are looking for was not found.</p>
               </div>
               <InputGroup className="input-prepend">
