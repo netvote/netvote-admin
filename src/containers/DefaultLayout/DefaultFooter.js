5e73327fd4f8dcff6f55887b649a45c5234c7782
Jonathan Alexander <jalex206@gmail.com>
2018-11-10 19:50:07 -0500

    adding back the powered by

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -15,8 +15,8 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href="https://citizendata.network">Citizen Data</a> &copy; 2018 Citizen Data Corp.</span>
-        // <span className="ml-auto">Powered by <a href="https://citizendata.network">Citizen Data Network</a></span>
+        <span>&copy; 2018 Citizen Data Corp.</span>
+        <span className="ml-auto">Powered by <a href="https://citizendata.network">Citizen Data Network</a></span>
       </React.Fragment>
     );
   }
