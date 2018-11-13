3f9d043fb7e575f4feed2e4e711d218387976d23
Keith Meyers <keith@netvote.io>
2018-11-13 10:02:31 -0500

    changed AWS settings

--- src/config/aws-settings.js
+++ src/config/aws-settings.js
@@ -2,8 +2,8 @@
 export const AWS_AUTH_REGION = 'us-east-1';
 
 //Amazon Cognito User Pool ID
-export const AWS_AUTH_USER_POOL_ID = 'us-east-1_gjH1FmMsn';
+export const AWS_AUTH_USER_POOL_ID = 'us-east-1_vLfOuHEEO'; 
 
 //Amazon Cognito Web Client ID (26-char alphanumeric string)
-export const AWS_AUTH_USER_POOL_WEB_CLIENT_ID = '2lqd49qru5jvlintj41nngtan';
+export const AWS_AUTH_USER_POOL_WEB_CLIENT_ID = '3us8atqlasfjh410gav0dt8a55';
 
