7cd4bf939c41b45f545e97bc9736b181dd33ffed
Keith Meyers <keith@netvote.io>
2018-11-29 11:00:40 -0500

    device adaptive signin

--- src/views/Pages/SignIn/SignIn.js
+++ src/views/Pages/SignIn/SignIn.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Input, Button, Card, CardGroup, CardBody, Col, Container, Form, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Input, Button, Card, CardFooter, CardGroup, CardBody, Col, Container, Form, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 import Auth from '@aws-amplify/auth'
 import { AuthPiece } from 'aws-amplify-react'
@@ -18,8 +18,16 @@ export default class SignIn extends AuthPiece {
     this.signIn = this.signIn.bind(this);
     this.onKeyDown = this.onKeyDown.bind(this);
 
+
+
     this._validAuthStates = ['signIn', 'signedOut', 'signedUp'];
-    this.state = {};
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
   }
 
   componentDidMount() {
@@ -28,7 +36,12 @@ export default class SignIn extends AuthPiece {
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.onKeyDown);
+    window.removeEventListener('resize', this.handleWindowSizeChange);
   }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
 
   onKeyDown(e) {
     if (e.keyCode !== 13) return;
@@ -91,75 +104,146 @@ export default class SignIn extends AuthPiece {
   }
 
   showComponent(theme) {
-    return (
-       <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <CardGroup>
-                <Card className="p-4">
-                  <CardBody>
-                    <Form>
-                      <Row className="justify-content-center">
-                        <Col md="4">
-                          <img src={logo} alt="logo" width="75" height="75" />
-                        </Col>
-                      </Row>
-                      <h1 align="center" style={{ fontSize: "20px", fontWeight: "bold", color: "#22b1dd" }}>Citizen Data Network</h1>
-                      <p align="center" className="text-muted">Sign In to your account</p>
-                      <InputGroup className="mb-3">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-envelope"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input
-                          autoFocus
-                          placeholder={I18n.get('Email')}
-                          key="username"
-                          name="username"
-                          onChange={this.handleInputChange}
-                        />
-                      </InputGroup>
-                      <InputGroup className="mb-4">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-lock"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input
-                          placeholder={I18n.get('Password')}
-                          type="password"
-                          key="password"
-                          name="password"
-                          onChange={this.handleInputChange}
-                        />
-                      </InputGroup>
-                      <Row>
-                        <Col xs="6">
-                          <Button onClick={this.signIn} color="primary" className="px-4">Login</Button>
-                        </Col>
-                        <Col xs="6" className="text-right">
-                          <Button color="link" onClick={() => this.changeState('forgotPassword')} className="px-0">Forgot password?</Button>
-                        </Col>
-                      </Row>
-                    </Form>
-                  </CardBody>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Leverage the blockchain for tamper-proof data and documents</p>
-                      <Button color="primary" onClick={() => this.changeState('signUp')} className="mt-3" active>Create free developer account</Button>
-                    </div>
-                  </CardBody>
-                </Card>
-              </CardGroup>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
+    const { width } = this.state;
+    // const isMobile = width <= 500;
+    const isMobile = width <= 992;
+
+    if (isMobile) {
+      return (
+        <div className="app flex-row align-items-center">
+          <Container>
+            <Row className="justify-content-center">
+              <Col md="8">
+                <CardGroup>
+                  <Card className="p-4">
+                    <CardBody>
+                      <Form>
+                        <Row align="center" className="justify-content-center" style={{ margin: "20px" }}>
+                          <Col md="4">
+                            <img align="center" src={logo} alt="logo" width="75" height="75" />
+                          </Col>
+                        </Row>
+                        <h1 align="center" style={{ fontSize: "20px", fontWeight: "bold", color: "#22b1dd", margin: "20px"  }}>Citizen Data Network</h1>
+                        <p align="center" className="text-muted"><Button align="center" color="link" onClick={() => this.changeState('signUp')} className="px-0">Create free developer account</Button></p>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-envelope"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            autoFocus
+                            placeholder={I18n.get('Email')}
+                            key="username"
+                            name="username"
+                            onChange={this.handleInputChange}
+                          />
+                        </InputGroup>
+                        <InputGroup className="mb-4">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-lock"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder={I18n.get('Password')}
+                            type="password"
+                            key="password"
+                            name="password"
+                            onChange={this.handleInputChange}
+                          />
+                        </InputGroup>
+                        <Row>
+                          <Col xs="6">
+                            <Button color="link" onClick={() => this.changeState('forgotPassword')} className="px-0">Forgot password?</Button>
+                          </Col>
+                          <Col xs="6" className="text-right">
+                            <Button onClick={this.signIn} color="primary" className="px-4">Login</Button>
+                          </Col>
+                        </Row>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                </CardGroup>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div className="app flex-row align-items-center">
+          <Container>
+            <Row className="justify-content-center">
+              <Col md="8">
+                <CardGroup>
+                  <Card className="p-4">
+                    <CardBody>
+                      <Form>
+                        <Row align="center" className="justify-content-center" style={{ margin: "20px" }}>
+                          <Col md="4">
+                            <img align="center" src={logo} alt="logo" width="75" height="75" />
+                          </Col>
+                        </Row>
+                        <h1 align="center" style={{ fontSize: "20px", fontWeight: "bold", color: "#22b1dd", margin: "20px" }}>Citizen Data Network</h1>
+                     
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-envelope"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            autoFocus
+                            placeholder={I18n.get('Email')}
+                            key="username"
+                            name="username"
+                            onChange={this.handleInputChange}
+                          />
+                        </InputGroup>
+                        <InputGroup className="mb-4">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-lock"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder={I18n.get('Password')}
+                            type="password"
+                            key="password"
+                            name="password"
+                            onChange={this.handleInputChange}
+                          />
+                        </InputGroup>
+                        <Row>
+                          <Col xs="6">
+                            <Button color="link" onClick={() => this.changeState('forgotPassword')} className="px-0">Forgot password?</Button>
+
+                          </Col>
+                          <Col xs="6" className="text-right">
+                            <Button onClick={this.signIn} color="primary" className="px-4">Login</Button>
+                          </Col>
+                        </Row>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                  <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
+
+                    <CardBody className="text-center">
+                      <div>
+                        <h2 style={{ fontWeight: "bold", margin: "20px" }}>Sign up</h2>
+                        <p>Leverage the blockchain for tamper-proof data and documents</p>
+                        <Button color="primary" onClick={() => this.changeState('signUp')} className="mt-3" active>Create free developer account</Button>
+                      </div>
+                    </CardBody>
+                  </Card>
+
+                </CardGroup>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      );
+    }
   }
 }
