4b0c5133dc92bbdd2a42be15fbcb5915fcdfbe56
Keith Meyers <keith@netvote.io>
2018-11-29 14:56:52 -0500

    Cognito/Amplify change password support

--- src/views/Pages/Profile/Profile.js
+++ src/views/Pages/Profile/Profile.js
@@ -15,7 +15,7 @@ import {
   ModalBody,
   ModalFooter,
   ModalHeader,
-  Form, FormGroup, Label, Input, FormFeedback, FormText 
+  Form, FormGroup, Label, Input, FormFeedback, FormText
 } from 'reactstrap';
 
 function getFormattedDate(created) {
@@ -30,11 +30,31 @@ class Profile extends Component {
 
     this.accountType = this.loadData.bind(this);
     this.toggleChangePasswordModal = this.toggleChangePasswordModal.bind(this);
+    this.toggleError = this.toggleError.bind(this);
+    this.toggleSuccess = this.toggleSuccess.bind(this);
+
+    //AWS Auth
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submit = this.submit.bind(this);
 
     this.state = {
       accountType: '',
     };
+
+    this.inputs = {};
   };
+
+  toggleSuccess() {
+    this.setState({
+      success: !this.state.success,
+    });
+  }
+
+  toggleError() {
+    this.setState({
+      error: !this.state.error,
+    });
+  }
 
   loadData = async () => {
     await Auth.currentSession()
@@ -50,9 +70,6 @@ class Profile extends Component {
       maxApiKeys: tenantInfo["maxApiKeys"],
       createdAt: tenantInfo["createdAt"],
     });
-
-    console.log('Account Type: ' + this.state.accountType);
-    console.log('CreatedAt: ' + this.state.createdAt);
   }
 
   componentDidMount = async () => {
@@ -66,11 +83,63 @@ class Profile extends Component {
   }
 
   onChangePasswordBtnClick = async (id) => {
-    console.log('CLICKED Change Password!');
-
     //Show Modal
     this.toggleChangePasswordModal();
+  }
 
+  handleInputChange(evt) {
+    const { name, value} = evt.target;
+
+    this.inputs = this.inputs || {};
+    this.inputs[name] = value;
+  }
+
+  submit() {
+    const { password, newPassword, confirmPassword } = this.inputs;
+
+    //Validate confirmation password match
+    if (newPassword !== confirmPassword) {
+      //Show Password Change Error
+      console.log('Change Password Error: Mismatch between passwords');
+
+      this.setState({
+        errorTitle: `Change Password`,
+        errorMessage: `Please ensure new passwords match`
+      });
+
+      this.toggleError();
+    } else {
+      //Get Authenticated User
+      Auth.currentAuthenticatedUser()
+        .then(user => {
+          //Change password
+          return Auth.changePassword(user, password, newPassword);
+        }).then(data => {
+          //Successful Password Change
+          console.log('Password Changed Successfully');
+
+          this.setState({
+            successTitle: `Change Password`,
+            successMessage: `Your password has been changed`
+          });
+
+          this.toggleSuccess();
+
+          //Hide Modal
+          this.toggleChangePasswordModal();
+        }).catch(err => {
+
+          //Show Password Change Error
+          console.log('Change Password Error: ' + err["message"]);
+
+          this.setState({
+            errorTitle: `Change Password`,
+            errorMessage: `${err["message"]}`
+          });
+
+          this.toggleError();
+        });
+    }
   }
 
   render() {
@@ -84,9 +153,9 @@ class Profile extends Component {
         <Row>
           <Col>
             <Card style={{ marginTop: 30 + 'px' }}>
-              {/* <CardHeader>
-                Current User Profile
-              </CardHeader> */}
+              <CardHeader>
+                User Profile
+                </CardHeader>
               <CardBody>
                 <Table responsive>
                   <thead>
@@ -111,59 +180,72 @@ class Profile extends Component {
           </Col>
         </Row>
 
-
         <Modal isOpen={this.state.primary} centered={true} toggle={this.toggleChangePasswordModal}
           className={'modal-primary ' + this.props.className} size="lg" color="primary">
           <ModalHeader toggle={this.toggleChangePasswordModal}><i className="fa fa-lock"></i>&nbsp;Change Password</ModalHeader>
           <ModalBody id="modalBodyText" style={{ margin: "20px" }} >
-          <Form>
-            <FormGroup>
-              <Label for="currentPassword">Current Password</Label>
-              {/* <Input invalid /> */}
+            <Form>
+              <FormGroup>
+                <Label for="currentPassword" style={{ fontWeight: "bold", color: "#22b1dd" }}>Current Password</Label>
                 <Input
                   placeholder='Current Password'
                   type="password"
                   key="password"
                   name="password"
-                  // onChange={this.handleInputChange}
+                  id="password"
+                  onChange={this.handleInputChange}
                 />
-              <FormFeedback>Error: Password is invalid!</FormFeedback>
-              <FormText>Enter your current password</FormText>
-            </FormGroup>
-            <FormGroup>
-              <Label for="newPassword">New Password</Label>
-              {/* <Input invalid /> */}
+                <FormFeedback>Error: Password is invalid!</FormFeedback>
+                <FormText>Enter your current password</FormText>
+              </FormGroup>
+              <FormGroup>
+                <Label for="newPassword" style={{ fontWeight: "bold", color: "#22b1dd" }}>New Password</Label>
                 <Input
                   placeholder='New Password'
                   type="password"
                   key="newPassword"
                   name="newPassword"
-                  // onChange={this.handleInputChange}
+                  id="newPassword"
+                  onChange={this.handleInputChange}
                 />
-              <FormFeedback>Error: Password is invalid!</FormFeedback>
-              <FormText>Confirm your password below</FormText>
-            </FormGroup>
-            <FormGroup>
-              <Label for="confirmPassword">Confirm Password</Label>
-              {/* <Input invalid /> */}
+                <FormFeedback>Error: Password is invalid!</FormFeedback>
+                <FormText>Confirm your password below</FormText>
+              </FormGroup>
+              <FormGroup>
+                <Label for="confirmPassword" style={{ fontWeight: "bold", color: "#22b1dd" }}>Confirm Password</Label>
                 <Input
                   placeholder='New Password'
                   type="password"
                   key="confirmPassword"
                   name="confirmPassword"
-                  // onChange={this.handleInputChange}
+                  onChange={this.handleInputChange}
                 />
-              <FormFeedback>Error: Password is invalid!</FormFeedback>
-              <FormText></FormText>
-            </FormGroup>
-          </Form>
+                <FormFeedback>Error: Password is invalid!</FormFeedback>
+                <FormText></FormText>
+              </FormGroup>
+            </Form>
           </ModalBody>
           <ModalFooter>
             <Button color="secondary" onClick={this.toggleChangePasswordModal}>Cancel</Button>
-            <Button color="primary" onClick={this.toggleChangePasswordModal}>Save</Button>
+            <Button color="primary" onClick={this.submit}>Save</Button>
           </ModalFooter>
         </Modal>
 
+        <Modal isOpen={this.state.success} centered={true} toggle={this.toggleSuccess}
+          className={'modal-success ' + this.props.className}>
+          <ModalHeader toggle={this.toggleSuccess}>{this.state.successTitle}</ModalHeader>
+          <ModalBody style={{ fontWeight: "bold", color: "green", backgroundColor: "#90ee9057" }}>
+            {this.state.successMessage}
+          </ModalBody>
+        </Modal>
+
+        <Modal isOpen={this.state.error} centered={true} toggle={this.toggleError}
+          className={'modal-danger ' + this.props.className}>
+          <ModalHeader toggle={this.toggleError}>{this.state.errorTitle}</ModalHeader>
+          <ModalBody style={{ fontWeight: "bold", color: "red", backgroundColor: "#ee909057" }}>
+            {this.state.errorMessage}
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
